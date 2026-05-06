debca1dc6324841732cafb06490a2e0e6a88e584
Jiten Dhull <jitendhull@proton.me>
2026-05-06 22:37:51 +0530

    Dictionary and its few methods

---
+++ Intermediate Python/03_met_museum.py
@@ -0,0 +1,6 @@
+museum_item = {'artist': 'Jacob Kuntz', 'period': 'Regency Era', 'date': '1810-20'}
+
+print('Information: ', museum_item)
+
+print('Keys: ', museum_item.keys())
+print('Values: ', museum_item.values())
