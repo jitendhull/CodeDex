eac6476b40011a49f987ed6820f3a118f3a1bcf3
Jiten Dhull <jitendhull@proton.me>
2026-05-12 22:04:23 +0530

    Wealth Manager with multiple tests

---
+++ Intermediate Python/Chapter-4/test_bank_account.py
@@ -0,0 +1,43 @@
+from bank_account import BankAccount
+import unittest
+
+class TestBankAccount(unittest.TestCase):
+    def setUp(self):
+        self.account = BankAccount.BankAccount(100)
+
+    def tearDown(self):
+        self.account = None
+
+    def test_initial_balance(self):
+        self.assertEqual(self.account.balance, 100)
+
+    def test_deposit_positive_amount(self):
+        self.account.deposit(50)
+        self.assertEqual(self.account.balance, 150)
+
+    def test_deposit_zero_amount(self):
+        with self.assertRaises(ValueError):
+            self.account.deposit(0)
+
+    def test_deposit_negative_amount(self):
+        with self.assertRaises(ValueError):
+            self.account.deposit(-40)
+
+    def test_withdraw_sufficient_funs(self):
+        self.account.withdraw(30)
+        self.assertRaises(self.account.balance, 70)
+
+    def test_withdraw_insufficient_funs(self):
+        with self.assertRaises(ValueError):
+            self.account.withdraw(200)
+
+    def test_withdraw_zero_amount(self):
+        with self.assertRaises(ValueError):
+            self.account.withdraw(0)
+
+    def test_withdraw_negative_amount(self):
+        with self.assertRaises(ValueError):
+            self.account.withdraw(-50)
+
+if __name__ == '__main__':
+    unittest.main()
