8d6eb8f2bbf067ac2e3440de52ebdab5242cdadd
Jiten Dhull <jitendhull@proton.me>
2026-05-21 21:15:14 +0530

    update: reorganize comments for clarity in ngrams.py

--- Gen-AI/ngrams.py
+++ Gen-AI/ngrams.py
@@ -1,11 +1,15 @@
+# Import nltk library and its tokenize and ngrams modules
 import nltk 
 from nltk.util import ngrams
 from nltk.tokenize import word_tokenize
 
+# Take user input for conversion into token
 sample_text = input("Enter a sentence: ")
+
+# Use nltk's word_tokenize function to split the input text into tokens
 tokens = word_tokenize(sample_text)
 
-
+# Generate unigrams, bigrams, trigrams, and n-grams using the ngrams function from nltk
 unigrams = list(ngrams(tokens, 1))
 
 bigrams = list(ngrams(tokens, 2))
@@ -14,6 +18,7 @@ trigrams = list(ngrams(tokens, 3))
 
 ngrams = list(ngrams(tokens, 4))
 
+# Print the tokens and the generated n-grams
 print("Token:", tokens)
 print("Unigrams:", unigrams)
 print("Bigrams:", bigrams)
