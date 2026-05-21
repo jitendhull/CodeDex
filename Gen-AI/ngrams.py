c3af44b090c5849af4c4dc504e101e0d1623c953
Jiten Dhull <jitendhull@proton.me>
2026-05-21 21:13:54 +0530

    update: add bigrams, trigrams, and n-grams functionality in ngrams.py

--- Gen-AI/ngrams.py
+++ Gen-AI/ngrams.py
@@ -5,5 +5,17 @@ from nltk.tokenize import word_tokenize
 sample_text = input("Enter a sentence: ")
 tokens = word_tokenize(sample_text)
 
+
 unigrams = list(ngrams(tokens, 1))
 
+bigrams = list(ngrams(tokens, 2))
+
+trigrams = list(ngrams(tokens, 3))
+
+ngrams = list(ngrams(tokens, 4))
+
+print("Token:", tokens)
+print("Unigrams:", unigrams)
+print("Bigrams:", bigrams)
+print("Trigrams:", trigrams)
+print("N-grams:", ngrams)
